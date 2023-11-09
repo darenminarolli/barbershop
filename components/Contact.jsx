9ef046cd6740ead25299f0dac0e651d475f40ff1
darenminarolli <80909893+darenminarolli@users.noreply.github.com>
2023-11-09 23:48:34 +0100

    changes

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -15,22 +15,22 @@ const Contact = () => {
             </h1>
          </hgroup>
          <div className='grid grid-cols-3'>
-            <p>Tel:0676320355</p>
+            <p>Tel:0683292847</p>
             <div className='mb-5 '>
               <Image src="/assets/images/logo-barber-removebg-preview.png" alt="logo" width={100} height={100} className='opacity-70' />
               <div className='flex  gap-5 pl-2'>
-              <a href="#">
+              <a href="https://instagram.com/profesional_sallon?igshid=MThtNXc1Nm0wend6eQ==">
               <svg class="h-8 w-8 text-red-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <rect x="4" y="4" width="16" height="16" rx="4" />  <circle cx="12" cy="12" r="3" />  <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
               </svg>
               </a>
-              <a href="#">
+              <a href="https://instagram.com/profesional_sallon?igshid=MThtNXc1Nm0wend6eQ==">
               <svg class="h-8 w-8 text-red-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" /></svg>
               </a>
               </div>
 
             </div>
-            <p className='text-sm sm:text-base'>Adresa: Rr.Gjergj Fishta, <br />
-            perball Big Market</p>
+            <p className='text-sm sm:text-base'>Adresa: Rr. Besim Imami, <br />
+            afer gjimnazit Arben Broci.</p>
          </div>
       </div>
     </section>
--- components/Home.jsx
+++ components/Home.jsx
@@ -1,24 +1,41 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Home = () => {
   return (
     <>
-    <section className='section_layout'>
+      <section className="section_layout">
+        <div className="flex flex-col justify-center items-center">
+          <Image
+            src="/assets/images/logo-barber-removebg-preview.png"
+            alt="logo"
+            width={130}
+            height={130}
+            className="opacity-70 "
+          />
+          <h1 className="text_header ">Berberi me i mire ne Tiron!</h1>
+          <h5 className="text-sm">Me eksperienc 10 vjecare ne treg.</h5>
+        </div>
+        <div className="container">
+          <Image
+            src="/assets/images/Img.jpg"
+            className="img rounded-md shadow-md "
+            width={250}
+            height={150}
+            alt="photo"
+          />
 
-    <div className='grid justify-center items-center'>     
-    <h1 className='text_header '>Berberi me i mire ne Tiron!</h1>
-     <h5 className='flex justify-center items-center text-sm'>Me eksperienc 10 vjecare ne treg.</h5>
-    </div>
-     <div className='container'>
-     <Image src='/assets/images/Img.jpg' className='img rounded-md shadow-md ' width={250} height={150} alt='photo'/>
-     
-     <Image src='/assets/images/Img2.jpg' className='img rounded-md shadow-md ' width={250} height={150} alt='photo'/>
+          <Image
+            src="/assets/images/Img2.jpg"
+            className="img rounded-md shadow-md "
+            width={250}
+            height={150}
+            alt="photo"
+          />
+        </div>
+      </section>
+    </>
+  );
+};
 
-     </div>
-    </section>
-     </>
-  )
-}
-
-export default Home
+export default Home;
